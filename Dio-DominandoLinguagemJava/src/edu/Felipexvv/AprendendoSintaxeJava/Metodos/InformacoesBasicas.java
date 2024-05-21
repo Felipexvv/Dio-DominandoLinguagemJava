70d246ab7829a250b9a8e6ca4038e2676b153e42
Felipexvv <felipexvv@gmail.com>
2024-05-21 15:41:29 -0300

    Conteudo sobre Metodos

---
+++ Dio-DominandoLinguagemJava/src/edu/Felipexvv/AprendendoSintaxeJava/Metodos/InformacoesBasicas.java
@@ -0,0 +1,26 @@
+package edu.Felipexvv.AprendendoSintaxeJava.Metodos;
+
+public class InformacoesBasicas {
+    public static void main(String[] args) {
+        
+        //Criterios de deficições de metodos
+
+        //Qual a proposta principal do metodo? - voce deve se perguntar constantemente ate compreender a real finalidade do mesmo;
+        //Qual o tipo de retorno esperado apos executar o metodo? - voce analisar se o metodo sera responsavel por retornar algum valor ou nao;
+
+        //Caso o metodo nao retorne nehum valor, ele sera representado pela palavra-chave void;
+
+        /* 
+            Outros Aspectos
+
+            * Quais os parametros serao necessarios para execução do metodo? os metodos as vezes precisão de argumentos como criterios para a execução;
+            
+            * O metodo possui o risco de apresentar alguma exceção? Excução são comuns na execução de metodos, as vezes é necessario prever e tratar a possivel existencia de uma execução;
+
+            * Qual a visibilidade do metodo? sera necessario que o metodo seja visivel a toda aplicação, somento em mesmo pacotes, atravex de herança ou somente a nivel a propria classe;
+
+        */
+
+    }
+
+}
