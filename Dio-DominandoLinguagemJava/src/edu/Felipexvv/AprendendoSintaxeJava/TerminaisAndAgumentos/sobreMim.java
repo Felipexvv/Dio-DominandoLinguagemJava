a5880808d684f4c00551850eeb473e02f7c16b85
Felipexvv <felipexvv@gmail.com>
2024-05-22 15:49:11 -0300

    adicionando conteudo sobre Scanner da aula de terminal

---
+++ Dio-DominandoLinguagemJava/src/edu/Felipexvv/AprendendoSintaxeJava/TerminaisAndAgumentos/sobreMim.java
@@ -0,0 +1,33 @@
+package edu.Felipexvv.AprendendoSintaxeJava.TerminaisAndAgumentos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class sobreMim {
+    public static void main(String[] args) {
+        //Ctriando o objeto scanner
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite o seu sobre nome: ");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura: ");
+        double altura = scanner.nextDouble();
+
+        
+        //Imprimindo os dados obtidos pelo usuario
+
+        System.out.println("Ola, me chamo " + nome + " " +  sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+
+
+    }
+}
